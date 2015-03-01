e5fc078d9477ca173ca92eceae42026606f197d9
william wright <william86378@williams-Mac.local>
2015-03-01 14:16:53 -0800

    Initial Commit

---
+++ reasosns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reasosns
+//
+//  Created by william wright  on 3/1/15.
+//  Copyright (c) 2015 A.R.C softwere and enginering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
